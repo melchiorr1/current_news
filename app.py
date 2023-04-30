9bec42a19a33eac577de9b220076106618c31f6f
Mikolaj <mikolajplaszczyca@gmail.com>
2023-04-30 21:37:29 +0200

    added a num feature to get_articles

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 import requests
 import os
 
-def get_articles():
+def get_articles(num=4):
     api_key = app.config['NYT_API_KEY']
     dic = {}
 
     response = requests.get(f'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={api_key}')
-    for article in response.json()['results'][:4]:
+    for article in response.json()['results'][:num]:
         dic[article['title']] = article['media'][0]['media-metadata'][-1]['url']
     return dic
 
@@ -17,5 +17,5 @@ app.config.from_pyfile('.env.py')
 
 @app.route('/')
 def index():
-    return render_template("index.html", articles=get_articles())
+    return render_template("index.html", articles=get_articles(8))
 
