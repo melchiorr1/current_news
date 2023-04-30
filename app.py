7386199b930038f73fab13b3f713fc2efad160e2
Mikolaj <mikolajplaszczyca@gmail.com>
2023-04-30 21:43:43 +0200

    added hrefs to titles

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def get_articles(num=4):
 
     response = requests.get(f'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={api_key}')
     for article in response.json()['results'][:num]:
-        dic[article['title']] = article['media'][0]['media-metadata'][-1]['url']
+        dic[article['title']] = [article['url'], article['media'][0]['media-metadata'][-1]['url']]
     return dic
 
 app = Flask(__name__)
