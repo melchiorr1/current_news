55625e3205adfe8f2bc39934a85484ebabac0423
Mikolaj <mikolajplaszczyca@gmail.com>
2023-05-03 17:46:45 +0200

    fixed a bug with loading articles

--- app.py
+++ app.py
@@ -8,13 +8,14 @@ def get_articles(num=4):
 
     response = requests.get(f'https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key={api_key}')
     for article in response.json()['results'][:num]:
-        dic[article['title']] = [article['url'], article['media'][0]['media-metadata'][-1]['url']]
+        if article['media']:
+            dic[article['title']] = [article['url'], article['media'][0]['media-metadata'][-1]['url']]
     return dic
 
 app = Flask(__name__)
-app.config.from_pyfile('.env.py')
+app.config.from_pyfile('env.py')
 
 @app.route('/')
 def index():
-    return render_template("index.html", articles=get_articles(8))
+    return render_template("index.html", articles=get_articles(16))
 
